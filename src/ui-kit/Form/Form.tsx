2e49221e5b300a2bd0c704ad8928b70e9ea10035
Maksim Sobolev <youneedmax@gmail.com>
2025-04-05 17:12:53 +0200

    feat: add `Form` component base

---
+++ src/ui-kit/Form/Form.tsx
@@ -0,0 +1,36 @@
+import type { FormEventHandler, ReactNode } from "react";
+
+type FormProps = {
+  title: string;
+  children: ReactNode;
+  submitText?: string;
+  canSubmit?: boolean;
+  onSubmit: FormEventHandler<HTMLFormElement>;
+};
+
+/**
+ * Renders a form.
+ */
+function Form(props: FormProps) {
+  const {
+    title,
+    children,
+    submitText = "Submit",
+    canSubmit = true,
+    onSubmit,
+  } = props;
+
+  return (
+    <section className="mx-auto max-w-sm p-6">
+      <h1>{title}</h1>
+      <form onSubmit={onSubmit}>
+        <div>{children}</div>
+        <button type="submit" disabled={!canSubmit}>
+          {submitText}
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Form;

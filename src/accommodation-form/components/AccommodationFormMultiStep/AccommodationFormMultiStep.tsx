ef01c6e22f0e6ff737a05852c1e544d328f8713c
Maksim Sobolev <youneedmax@gmail.com>
2025-04-05 16:21:29 +0200

    feat: dispatch custom event

--- src/accommodation-form/components/AccommodationFormMultiStep/AccommodationFormMultiStep.tsx
+++ src/accommodation-form/components/AccommodationFormMultiStep/AccommodationFormMultiStep.tsx
@@ -1,8 +1,32 @@
+import { useRef, useState } from "react";
+import type { FormEventHandler } from "react";
+
 /**
  * Multi-Step Accommodation Creation Form.
  */
 function AccommodationFormMultiStep() {
-  return <h1>Multi-Step Accommodation Creation Form.</h1>;
+  const formRef = useRef<HTMLFormElement>(null);
+  const [value, setValue] = useState("");
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const submitEvent = new CustomEvent("accommodation-creation-form-submit", {
+      detail: { value },
+      bubbles: true,
+      // propagate across the shadow DOM boundary
+      composed: true,
+    });
+
+    formRef.current?.dispatchEvent(submitEvent);
+  };
+
+  return (
+    <form ref={formRef} onSubmit={onSubmit}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default AccommodationFormMultiStep;

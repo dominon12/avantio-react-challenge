14defb78da26dc591f5addb0409394fdbf8542c1
Maksim Sobolev <youneedmax@gmail.com>
2025-04-05 19:08:16 +0200

    feat: use `ImageUploader` in `AccommodationForm`

--- src/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
+++ src/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 import Form from "src/ui-kit/Form";
 import FormField from "src/ui-kit/FormField";
+import ImageUploader from "src/ui-kit/ImageUploader";
 import Input from "src/ui-kit/Input";
 import Select from "src/ui-kit/Select";
 import TextArea from "src/ui-kit/TextArea";
@@ -9,7 +12,18 @@ import TextArea from "src/ui-kit/TextArea";
  * about an accommodation.
  */
 function AccommodationForm() {
+  const [images, setImages] = useState<{ id: string; value: File }[]>([]);
+
   const onSubmit = () => {};
+
+  const onImageUpload = (image: File) => {
+    const newId = `id-${Math.random().toString(36)}-${Date.now()}`;
+    setImages((prev) => prev.concat({ id: newId, value: image }));
+  };
+
+  const onImageRemove = (id: string) => {
+    setImages((prev) => prev.filter((image) => image.id !== id));
+  };
 
   return (
     <Form title="Accommodation" onSubmit={onSubmit}>
@@ -27,6 +41,17 @@ function AccommodationForm() {
             <option value="villa">Villa</option>
             <option value="house">House</option>
           </Select>
+        )}
+      </FormField>
+      <FormField label="Photos">
+        {(fieldId) => (
+          <ImageUploader
+            inputId={fieldId}
+            images={images}
+            onUpload={onImageUpload}
+            onRemove={onImageRemove}
+            maxImages={2}
+          />
         )}
       </FormField>
     </Form>

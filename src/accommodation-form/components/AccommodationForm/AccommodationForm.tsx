b77535f0dc348b3654539195c3b8eaacbbedb86c
Maksim Sobolev <youneedmax@gmail.com>
2025-04-05 18:20:48 +0200

    feat: use `Select` component in `AccommodationForm`

--- src/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
+++ src/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
@@ -1,6 +1,7 @@
 import Form from "src/ui-kit/Form";
 import FormField from "src/ui-kit/FormField";
 import Input from "src/ui-kit/Input";
+import Select from "src/ui-kit/Select";
 import TextArea from "src/ui-kit/TextArea";
 
 /**
@@ -17,6 +18,16 @@ function AccommodationForm() {
       </FormField>
       <FormField label="Description">
         {(fieldId) => <TextArea id={fieldId} placeholder="Description" />}
+      </FormField>
+      <FormField label="Type">
+        {(fieldId) => (
+          <Select id={fieldId}>
+            <option value="">Select...</option>
+            <option value="apartment">Apartment</option>
+            <option value="villa">Villa</option>
+            <option value="house">House</option>
+          </Select>
+        )}
       </FormField>
     </Form>
   );

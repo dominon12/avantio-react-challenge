c8eb91d0750490434799ed193cec24364bc13cf1
Maksim Sobolev <youneedmax@gmail.com>
2025-04-05 18:20:34 +0200

    feat: add `Select` component

---
+++ src/ui-kit/Select/Select.tsx
@@ -0,0 +1,16 @@
+import type { ComponentPropsWithoutRef } from "react";
+
+import { inputBaseClass } from "../shared/input-styles";
+
+type BaseSelectProps = ComponentPropsWithoutRef<"select">;
+
+type SelectProps = Omit<BaseSelectProps, "className">;
+
+/**
+ * Select component.
+ */
+function Select(props: SelectProps) {
+  return <select className={inputBaseClass} {...props} />;
+}
+
+export default Select;

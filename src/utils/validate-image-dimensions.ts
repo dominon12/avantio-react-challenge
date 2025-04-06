98c4b0d3fb2ad02bb22be29f9bf7bb85a7339f56
Maksim Sobolev <youneedmax@gmail.com>
2025-04-06 20:35:27 +0200

    feat: add `validate-image-dimensions` util

---
+++ src/utils/validate-image-dimensions.ts
@@ -0,0 +1,34 @@
+type ImageDimensionsValidatorParams = {
+  image: File;
+  maxWidth: number;
+  maxHeight: number;
+};
+
+/**
+ * Validates image dimensions.
+ */
+async function validateImageDimensions(
+  params: ImageDimensionsValidatorParams
+): Promise<boolean> {
+  const { image, maxWidth, maxHeight } = params;
+
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    const imageUrl = URL.createObjectURL(image);
+
+    img.onload = () => {
+      const isValid = img.width <= maxWidth && img.height <= maxHeight;
+      URL.revokeObjectURL(imageUrl);
+      resolve(isValid);
+    };
+
+    img.onerror = () => {
+      URL.revokeObjectURL(imageUrl);
+      reject(false);
+    };
+
+    img.src = imageUrl;
+  });
+}
+
+export default validateImageDimensions;

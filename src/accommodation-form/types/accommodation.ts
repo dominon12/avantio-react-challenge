277cd4ffe7c9c91285d965633a659790fda7d5f6
Maksim Sobolev <youneedmax@gmail.com>
2025-04-06 11:34:11 +0200

    feat: add `Accommodation` type

---
+++ src/accommodation-form/types/accommodation.ts
@@ -0,0 +1,11 @@
+import AccommodationType from "./accommodation-type";
+
+type Accommodation = {
+  name: string;
+  address: string;
+  description?: string;
+  type: AccommodationType;
+  photos: { id: string; file: File }[];
+};
+
+export default Accommodation;

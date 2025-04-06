a9bb8512bcf5d051ca294f501cac531a4e32e29f
Maksim Sobolev <youneedmax@gmail.com>
2025-04-06 22:37:22 +0200

    feat: render red asterisk in `Label` if `FormField` is required

--- src/ui-kit/FormField/FormField.tsx
+++ src/ui-kit/FormField/FormField.tsx
@@ -7,6 +7,7 @@ import FieldError from "../FieldError";
 type FormFieldProps = {
   label: string;
   children: (inputId: string) => ReactNode;
+  isRequired?: boolean;
   error?: ReactNode;
 };
 
@@ -15,13 +16,15 @@ type FormFieldProps = {
  * that renders a label.
  */
 function FormField(props: FormFieldProps) {
-  const { label, children, error } = props;
+  const { label, children, isRequired, error } = props;
 
   const id = useId();
 
   return (
     <div className="flex flex-col gap-1">
-      <Label htmlFor={id}>{label}</Label>
+      <Label htmlFor={id} isRequired={isRequired}>
+        {label}
+      </Label>
       {children(id)}
       {error && <FieldError>{error}</FieldError>}
     </div>
--- src/ui-kit/Label/Label.tsx
+++ src/ui-kit/Label/Label.tsx
@@ -2,17 +2,24 @@ import type { ComponentPropsWithoutRef } from "react";
 
 type BaseLabelProps = ComponentPropsWithoutRef<"label">;
 
-type LabelProps = Omit<BaseLabelProps, "className">;
+type LabelProps = Omit<BaseLabelProps, "className"> & {
+  isRequired?: boolean;
+};
 
 /**
  * Form field label.
  */
 function Label(props: LabelProps) {
+  const { isRequired, children, ...labelProps } = props;
+
   return (
     <label
       className="text-sm font-medium text-gray-700 cursor-pointer"
-      {...props}
-    />
+      {...labelProps}
+    >
+      {children}
+      {isRequired && <span className="text-red-500">*</span>}
+    </label>
   );
 }
 

626f4590592f5cc302b20616f0132aff9d7afa37
Maksim Sobolev <youneedmax@gmail.com>
2025-04-06 22:35:07 +0200

    feat: connect `OwnerForm` form

--- src/modules/owner-form/components/OwnerForm/OwnerForm.tsx
+++ src/modules/owner-form/components/OwnerForm/OwnerForm.tsx
@@ -1,15 +1,34 @@
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import type { SubmitHandler } from "react-hook-form";
+
 import Form from "src/ui-kit/Form";
+import ownerSchema from "../../schema/owner";
+import type Owner from "../../types/owner";
 
 /**
  * Owner form.
  */
 function OwnerForm() {
-  const onSubmit = () => {};
+  const form = useForm<Owner>({
+    mode: "onBlur",
+    resolver: yupResolver(ownerSchema),
+  });
+
+  const onSubmit: SubmitHandler<Owner> = (data) => {
+    console.log("submitted data", data);
+  };
 
   return (
-    <Form title="Owner" submitText="Next" onSubmit={onSubmit}>
-      Owner form
-    </Form>
+    <FormProvider {...form}>
+      <Form
+        title="Owner"
+        submitText="Next"
+        canSubmit={form.formState.isValid}
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+      </Form>
+    </FormProvider>
   );
 }
 

fad970fbc5921d20f31e18b162025356d688694e
Maksim Sobolev <youneedmax@gmail.com>
2025-04-06 20:44:44 +0200

    feat: use pointer cursor on `Button`

--- src/ui-kit/Button/Button.tsx
+++ src/ui-kit/Button/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = Omit<BaseButtonProps, "className">;
 function Button(props: ButtonProps) {
   return (
     <button
-      className="px-4 py-2 bg-blue-600 text-white font-semibold rounded transition 
+      className="px-4 py-2 bg-blue-600 text-white font-semibold rounded transition cursor-pointer
                hover:bg-blue-700 
                disabled:bg-gray-400 disabled:text-gray-100 disabled:cursor-not-allowed disabled:opacity-70"
       {...props}

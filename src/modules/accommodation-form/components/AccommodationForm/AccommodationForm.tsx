bab1bda6655a85027badf7088a4d5cedba98d80f
Maksim Sobolev <youneedmax@gmail.com>
2025-04-07 17:36:05 +0200

    feat: navigate to the next step on `AccommodationForm` submit

--- src/modules/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
+++ src/modules/accommodation-form/components/AccommodationForm/AccommodationForm.tsx
@@ -9,6 +9,8 @@ import DescriptionTextArea from "../DescriptionTextArea";
 import AccommodationTypeSelect from "../AccommodationTypeSelect";
 import ImageUploader from "../ImageUploader";
 import accommodationSchema from "src/modules/accommodation-form/schema/accommodation";
+import useAccommodationFormContext from "src/modules/accommodation-form-full/context/selector";
+import ActionType from "src/modules/accommodation-form-full/store/action-type";
 import type Accommodation from "src/modules/accommodation-form/types/accommodation";
 
 /**
@@ -16,13 +18,15 @@ import type Accommodation from "src/modules/accommodation-form/types/accommodati
  * about an accommodation.
  */
 function AccommodationForm() {
+  const { dispatch } = useAccommodationFormContext();
+
   const form = useForm<Accommodation>({
     mode: "onBlur",
     resolver: yupResolver(accommodationSchema),
   });
 
-  const onSubmit: SubmitHandler<Accommodation> = (data) => {
-    console.log("submitted data", data);
+  const onSubmit: SubmitHandler<Accommodation> = (payload) => {
+    dispatch({ type: ActionType.AccommodationFormSubmitted, payload });
   };
 
   return (

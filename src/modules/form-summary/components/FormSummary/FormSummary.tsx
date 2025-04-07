dec04d55dce28eaed07cc10a9b4d2450f1a37209
Maksim Sobolev <youneedmax@gmail.com>
2025-04-07 18:51:12 +0200

    feat: navigate to confirmation step upon `FormSummary` submission

--- src/modules/form-summary/components/FormSummary/FormSummary.tsx
+++ src/modules/form-summary/components/FormSummary/FormSummary.tsx
@@ -1,16 +1,29 @@
+import type { FormEventHandler } from "react";
+
 import useAccommodationFormContext from "src/modules/accommodation-form-full/context/selector";
 import Form from "src/ui-kit/Form";
 import AccommodationData from "../AccommodationData";
 import OwnerData from "../OwnerData";
+import ActionType from "src/modules/accommodation-form-full/store/action-type";
+import randomChoice from "src/utils/random-choice";
+import SubmissionResult from "src/modules/submission-confirmation/types/submission-result";
 
 /**
  * Summary of the accommodation
  * and owner form steps.
  */
 function FormSummary() {
-  const { state } = useAccommodationFormContext();
+  const { state, dispatch } = useAccommodationFormContext();
 
-  const onSubmit = () => {};
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    // get random submission result
+    const result = randomChoice(Object.values(SubmissionResult));
+
+    // update context value
+    dispatch({ type: ActionType.FormSummaryAccepted, payload: { result } });
+  };
 
   return (
     <Form title="Summary" onSubmit={onSubmit}>

b49415275e62e40b638fb6014ca734f61b80823b
Maksim Sobolev <youneedmax@gmail.com>
2025-04-07 17:35:45 +0200

    feat: render correct form step based on the state

--- src/modules/accommodation-form-full/components/AccommodationFormMultiStep/AccommodationFormMultiStep.tsx
+++ src/modules/accommodation-form-full/components/AccommodationFormMultiStep/AccommodationFormMultiStep.tsx
@@ -1,10 +1,30 @@
+import type { FC } from "react";
+
 import AccommodationFormSummary from "src/modules/accommodation-form-summary/components/AccommodationFormSummary";
+import FormStep from "../../types/form-step";
+import AccommodationForm from "src/modules/accommodation-form/components/AccommodationForm";
+import OwnerForm from "src/modules/owner-form/components/OwnerForm";
+import useAccommodationFormContext from "../../context/selector";
+
+// steps matched to components to render
+const steps: Record<FormStep, FC> = {
+  [FormStep.Accommodation]: AccommodationForm,
+  [FormStep.Owner]: OwnerForm,
+  [FormStep.Summary]: AccommodationFormSummary,
+  [FormStep.Confirmation]: () => <>confirmation</>,
+};
 
 /**
  * Multi-Step Accommodation Creation Form.
  */
 function AccommodationFormMultiStep() {
-  return <AccommodationFormSummary />;
+  const {
+    state: { step },
+  } = useAccommodationFormContext();
+
+  const StepComponent = steps[step];
+
+  return <StepComponent />;
 }
 
 export default AccommodationFormMultiStep;

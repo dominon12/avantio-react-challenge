5b5f4133b77d54c1b4f557e5dcf163abc3543e4c
Maksim Sobolev <youneedmax@gmail.com>
2025-04-08 18:33:41 +0200

    feat: allow `Button` to receive `className` prop

--- src/ui-kit/Button/Button.tsx
+++ src/ui-kit/Button/Button.tsx
@@ -2,16 +2,18 @@ import type { ComponentPropsWithoutRef } from "react";
 
 type BaseButtonProps = ComponentPropsWithoutRef<"button">;
 
-type ButtonProps = Omit<BaseButtonProps, "className">;
+type ButtonProps = BaseButtonProps;
 
 /**
  * Primary button.
  */
 function Button(props: ButtonProps) {
+  const { className, ...buttonProps } = props;
+
   return (
     <button
-      className="cursor-pointer rounded bg-blue-600 px-4 py-2 font-semibold text-white transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-100 disabled:opacity-70"
-      {...props}
+      className={`cursor-pointer rounded bg-blue-600 px-4 py-2 font-semibold text-white transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-100 disabled:opacity-70 ${className}`}
+      {...buttonProps}
     />
   );
 }

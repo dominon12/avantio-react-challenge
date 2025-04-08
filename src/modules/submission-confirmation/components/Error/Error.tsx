d85674d8926b799815d838b8549e834832d69729
Maksim Sobolev <youneedmax@gmail.com>
2025-04-08 18:09:11 +0200

    feat: require `action` property on `Error` and `Success` components

--- src/modules/submission-confirmation/components/Error/Error.tsx
+++ src/modules/submission-confirmation/components/Error/Error.tsx
@@ -1,15 +1,21 @@
 import Feedback from "src/ui-kit/Feedback";
+import { FeedbackProps } from "src/ui-kit/Feedback/Feedback";
+
+type ErrorProps = Required<Pick<FeedbackProps, "action">>;
 
 /**
  * Error form submission
  * confirmation.
  */
-function Error() {
+function Error(props: ErrorProps) {
+  const { action } = props;
+
   return (
     <Feedback
       title="Error!"
       message="Couldn't submit the form. Please try again later."
       kind="error"
+      action={action}
     />
   );
 }
--- src/modules/submission-confirmation/components/Success/Success.tsx
+++ src/modules/submission-confirmation/components/Success/Success.tsx
@@ -1,15 +1,21 @@
 import Feedback from "src/ui-kit/Feedback";
+import { FeedbackProps } from "src/ui-kit/Feedback/Feedback";
+
+type SuccessProps = Required<Pick<FeedbackProps, "action">>;
 
 /**
  * Successful form submission
  * confirmation.
  */
-function Success() {
+function Success(props: SuccessProps) {
+  const { action } = props;
+
   return (
     <Feedback
       title="Success!"
       message="The form has been submitted successfully"
       kind="success"
+      action={action}
     />
   );
 }

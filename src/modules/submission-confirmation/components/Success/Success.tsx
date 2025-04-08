4e1641418839a531b5e355dcf2836229a27b4d0b
Maksim Sobolev <youneedmax@gmail.com>
2025-04-08 18:50:50 +0200

    feat: implement form reset in `Success`

--- src/modules/submission-confirmation/components/Success/Success.tsx
+++ src/modules/submission-confirmation/components/Success/Success.tsx
@@ -1,3 +1,6 @@
+import useAccommodationFormContext from "src/modules/accommodation-form-full/context/selector";
+import ActionType from "src/modules/accommodation-form-full/store/action-type";
+import Button from "src/ui-kit/Button";
 import Feedback from "src/ui-kit/Feedback";
 
 /**
@@ -5,12 +8,21 @@ import Feedback from "src/ui-kit/Feedback";
  * confirmation.
  */
 function Success() {
+  const { dispatch } = useAccommodationFormContext();
+
+  /**
+   * Resets form to the initial state
+   * so that the use could
+   * add another accommodation.
+   */
+  const onReset = () => dispatch({ type: ActionType.ResetForm });
 
   return (
     <Feedback
       title="Success!"
       message="The form has been submitted successfully"
       kind="success"
+      action={<Button onClick={onReset}>Add another accommodation</Button>}
     />
   );
 }
